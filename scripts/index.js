54b80aaf357ce051a72f81dbb29992a25764f4e2
gustavimm <gustavomachadoespindola49@gmail.com>
2025-09-11 21:19:34 -0300

    feat(ui): Implement filtering logic and ensure responsiveness
    This commit introduces two major updates: the full functionality for task filtering and the implementation of a responsive design for mobile devices.
    
    Filtering Logic:
    - Adds a single event listener to the filter controls using event delegation.
    - Implements a `switch` statement to filter tasks based on their completion status (`.completed` class).
    - Dynamically toggles task visibility by manipulating the `display` property.
    
    Responsiveness:
    - Uses CSS Media Queries to adapt the layout for screens up to 640px.
    - Adjusts typography, spacing, and form layout to prevent overflow and improve usability on mobile.
    - Fixes layout issues by resetting desktop-specific styles (e.g., margins) on smaller screens.

--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,53 @@ import  { createTask, showSuccessMessage } from './task.js';
 const taskForm = document.querySelector('form'); //Criando uma variável para o formulário
 const taskInput = document.querySelector('#task-input'); //Criando uma variável para o input de tarefa
 const taskList = document.querySelector('#task-list'); //Criando uma variável para a lista de tarefas
+const taskFilters = document.querySelector('#task-filters'); //Criando uma variável para os botões de filtro
+
+
+taskFilters.addEventListener('click', (event) => {
+ const filter = event.target.dataset.filter; // Obtém o filtro do botão clicado
+
+ // 1. pega todos os itens da lista
+ const tasks = taskList.querySelectorAll('li');
+
+ // 2. passa por cada tarefa li uma por uma
+
+ tasks.forEach(task => {
+    switch (filter) {
+        case 'todas':
+            // o que fazer quando o filtro for "todas"
+            task.style.display = 'flex'; // Mostra todas as tarefas
+            break;
+
+
+        case 'pendentes':
+            // o que fazer quando o filtro for "pendentes"
+            if (task.classList.contains('completed')) {
+                task.style.display = 'none'; // Esconde tarefas concluídas
+            } else {
+                // se não estiver concluída, mostra
+                task.style.display = 'flex'; // Mostra a tarefa
+            }
+            break;
+
+
+            
+        case 'concluidas':
+
+        if (task.classList.contains('completed')) {
+            // se a tarefa está completada, então mostra
+            task.style.display = 'flex'; // Mostra a tarefa
+        } else {
+            task.style.display = 'none'; // Esconde tarefas pendentes
+        }
+            // o que fazer quando o filtro for "concluidas"
+            break;
+
+    }
+ });
+
+ 
+});
 
 //Event Listener para o formulário
 taskForm.addEventListener("submit", (event) =>{
